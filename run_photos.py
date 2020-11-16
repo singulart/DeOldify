778b72319af441f37ef216b648084e6ac46e1236
Singulart <serpian@gmail.com>
2020-11-16 14:19:11 +0300

    batch process photos

--- run_photos.py
+++ run_photos.py
@@ -1,8 +1,7 @@
 from deoldify import device
 from deoldify.device_id import DeviceId
-#choices:  CPU, GPU0...GPU7
+import glob
 device.set(device=DeviceId.GPU0)
-import sys
 import torch
 
 if not torch.cuda.is_available():
@@ -18,14 +17,23 @@ warnings.filterwarnings("ignore", category=UserWarning, message=".*?Your .*? set
 
 colorizer = get_image_colorizer(artistic=True)
 
-source_url = sys.argv[1] #@param {type:"string"}
-print(source_url)
-render_factor = 35  #@param {type: "slider", min: 7, max: 40}
-watermarked = False #@param {type:"boolean"}
+source_folder = sys.argv[1]
+render_factor = 35
+watermarked = False
 
-if source_url is not None and source_url !='':
-    image_path = colorizer.plot_transformed_image_from_url(url=source_url, render_factor=render_factor, compare=True, watermarked=watermarked)
-    print("Image path %s" % image_path)
-    colorizer._save_result_image(image_path)
+if source_folder is not None and source_folder != '':
+    src_path = Path(source_folder)
+
+    for g in src_path.glob('**/*'):
+        for rf in range(8, 41, 2):
+            image_path = colorizer.plot_transformed_image(str(g), render_factor=rf, compare=True, watermarked=watermarked)
+            suffix = image_path.suffix
+            stem = image_path.stem
+            parent = g.parent
+            new_name = "".join([str(stem), '_', str(rf) + suffix])
+
+            new_place = Path(parent, new_name)
+            image_path.rename(new_place)
+            print('Saved to %s' % str(new_place))
 else:
-    print('Provide an image url and try again.')
+    print('Usage: python run_photos.py /path/to/folder/with/bw/images')
